478917d558792519e13aa4367e5f387ffb46c962
pavel_leshkin <leshkin_pashka@bk.ru>
2021-06-18 17:19:10 +0300

    master

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+projects:
+ - title: 'Семья'
+   todos:
+    - text: 'Купить молоко'
+      isCompleted: false
+    - text: 'Заменить масло в двигателе до 23 апреля'
+      isCompleted: false
+    - text: 'Отправить письмо бабушке'
+      isCompleted: true
+    - text: 'Заплатить за квартиру'
+      isCompleted: false
+    - text: 'Забрать обувь из ремонта'
+      isCompleted: false
+ - title: 'Работа'
+   todos:
+    - text: 'Позвонить заказчику'
+      isCompleted: true
+    - text: 'Отправить документы'
+      isCompleted: true
+    - text: 'Заполнить отчет'
+      isCompleted: false
+ - title: 'Прочее'
+   todos:
+    - text: 'Позвонить другу'
+      isCompleted: false
+    - text: 'Подготовиться к поездке'
+      isCompleted: false
